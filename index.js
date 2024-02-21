9881c08857753962a1950772d70e03caa4dfe4ec
Maheanuu Allain <maheanuu.allain@gmail.com>
2024-02-21 10:17:57 +0100

    refactor: test 3

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function soustraire(a, b) {
 const resultatAddition = additionner(5, 3);
 const resultatSoustraction = soustraire(10, 4);
 const test = soustraire(15, 4);
+const test2 = soustraire(20, 4);
 
 // Affichage des résultats dans la console
 console.log("Résultat de l'addition :", resultatAddition);
