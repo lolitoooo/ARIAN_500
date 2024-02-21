43ace643b28cb4abd5879e193372ee527a9fa280
Maheanuu Allain <maheanuu.allain@gmail.com>
2024-02-21 10:13:33 +0100

    feat: ajout de l'index.js pour test

---
+++ index.js
@@ -0,0 +1,17 @@
+// Déclaration d'une fonction pour ajouter deux nombres
+function additionner(a, b) {
+    return a + b;
+}
+
+// Déclaration d'une fonction pour soustraire deux nombres
+function soustraire(a, b) {
+    return a - b;
+}
+
+// Utilisation des fonctions définies ci-dessus
+const resultatAddition = additionner(5, 3);
+const resultatSoustraction = soustraire(10, 4);
+
+// Affichage des résultats dans la console
+console.log("Résultat de l'addition :", resultatAddition);
+console.log("Résultat de la soustraction :", resultatSoustraction);
